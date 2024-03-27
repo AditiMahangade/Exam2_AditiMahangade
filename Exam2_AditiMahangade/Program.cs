c8671c424ee69df91901dc65f8ea4760095f138b
Aditi Mahangade <Aditi.A.Mahangade-1@ou.edu>
2024-03-26 22:24:59 -0500

    Add project files.

---
+++ Exam2_AditiMahangade/Program.cs
@@ -0,0 +1,10 @@
+﻿// MIS 3013 005
+// Exam 2
+// March 27, 2024
+// Aditi Mahangade
+// 113607539
+
+Console.WriteLine("Exam 2");
+Console.WriteLine("Aditi Mahangade");
+Console.WriteLine("[Exam 2 code]"); // You are going to get the exam 2 code during the exam.
+
